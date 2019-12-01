4bd2615fd5b857af878c5088591a5913e10a0a20
longtean <longtean21@gmail.com>
2019-12-01 23:10:17 +0800

    feat:Implementation of basic request functions

---
+++ project/ts-axios/src/index.ts
@@ -0,0 +1,33 @@
+import { AxiosRequestConfig } from "./types";
+import xhr from './xhr'
+import { buildURL } from './helper/url'
+import { transformRequest } from './helper/data'
+import { processHeaders } from './helper/headers'
+
+
+function axios(config:AxiosRequestConfig){
+	processConfig(config)
+  	xhr(config)
+}
+
+function processConfig (config:AxiosRequestConfig) :void{
+	  config.url = transformRL(config)
+	  config.headers = transformHeaders(config)
+	  config.data = transformRequestData(config)
+}
+
+function transformRL( config :AxiosRequestConfig) :string {
+	const {url,params} = config
+	return buildURL(url,params)
+}
+
+function transformRequestData ( config:AxiosRequestConfig) :any{
+	return transformRequest(config.data)
+}
+
+function transformHeaders( config:AxiosRequestConfig):any {
+	const {headers={},data} = config
+	return processHeaders(headers,data)
+}
+
+export default axios
---
+++ project/ts-axios/src/xhr.ts
@@ -0,0 +1,19 @@
+import { AxiosRequestConfig } from "./types";
+
+export default function xhr(config:AxiosRequestConfig){
+  const { data=null,url,method='get',headers } = config
+
+  const request = new XMLHttpRequest()
+  
+  request.open(method.toUpperCase(), url, true )
+
+  Object.keys(headers).forEach(name =>{
+    if(data === null && name.toLocaleLowerCase() === 'content-type'){
+      delete headers[name]
+    } else {
+      request.setRequestHeader(name,headers[name])
+    }
+  })
+
+  request.send(data)
+}
